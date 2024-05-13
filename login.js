88edad5dd04ad061b5a7d1110b6a81bd71d83bbc
OddPenguinJojo <109307449+OddPenguinJojo@users.noreply.github.com>
2024-05-13 00:10:43 -0600

    wa

---
+++ login.js
@@ -0,0 +1,16 @@
+function ValidarUsuario() {
+    var usuario = document.getElementById("usuario").value;
+    var contraseña = document.getElementById("contraseña").value;
+  
+    if (usuario === "" || contraseña === "") {
+      alert("Por favor, complete todos los campos.");
+      return false; 
+    }
+  
+    return true;
+  }
+
+
+function redireccionarPagInicio() {
+    window.location.href = "PagInicio.html";
+}
