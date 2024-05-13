a9a575934dbb4450c43b705f3a3174b17da32457
LeeGisKer <ax_gil@hotmail.com>
2024-05-13 01:58:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,22 @@ app=Flask(__name__)
 
 def get_db_connection():
     connection = pymysql.connector.connect(host='192.168.0.10',
-                                         user='leegisker,',
+                                         user='leegisker',
                                          password='8563',
                                          db='sop',
                                          charset='utf8mb4',
                                          cursorclass=pymysql.cursors.DictCursor)
     return connection
 
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM sop')
+    items = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', items=items)
 
 @app.route('/api/password', methods=['GET'])
 def get_password():
@@ -21,16 +30,6 @@ def get_password():
     cursor.close()
     conn.close()
     return jsonify(passwords)
-
-
-
-def index():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM sop')
-    items = cursor.fetchall()
-    conn.close()
-    return render_template('index.html', items=items)
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=8080 )
