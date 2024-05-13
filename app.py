3849e690f316293c319259116f83938e232cf5f7
LeeGisKer <ax_gil@hotmail.com>
2024-05-13 01:54:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import pymysql
 app=Flask(__name__)
 
 def get_db_connection():
-    connection = pymysql.connector.connect(host='localhost',
-                                         user='root,',
-                                         password='1234',
-                                         db='sop')
+    connection = pymysql.connector.connect(host='192.168.0.10',
+                                         user='leegisker,',
+                                         password='8563',
+                                         db='sop',
+                                         charset='utf8mb4',
+                                         cursorclass=pymysql.cursors.DictCursor)
     return connection
 
 
-@app.route('/')
+@app.route('/api/password', methods=['GET'])
+def get_password():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM sop')
+    passwords = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(passwords)
+
+
+
 def index():
     conn = get_db_connection()
     cursor = conn.cursor()
