f8fd8254fc9279925dd0564f6d353052aba416e8
LeeGisKer <ax_gil@hotmail.com>
2024-05-13 02:45:41 -0600

    pro

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import pymysql
 app=Flask(__name__)
+app.secret_key='8563'
 
 def get_db_connection():
     connection = pymysql.connector.connect(host='192.168.0.10',
@@ -31,6 +32,57 @@ def get_password():
     conn.close()
     return jsonify(passwords)
 
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'usuario_predeterminado' and password == 'contraseña_predeterminada':
+            session['logged_in']=True
+            return redirect(url_for('managepasswords'))
+        else:
+            return 'Credenciales incorrectas'
+    return render_template('login.html')
+
+
+    
+    
+@app.route('/logout')
+def logout():
+    session['logged_in']=False
+    return redirect(url_for('login'))
+
+
+@app.route('/add_password',methods=['POST','GET'])
+def add_password():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    if request.method=='POST':
+        website = request.form['website']
+        password = request.form['password']
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO passwords (website, password) VALUES (%s, %s)', (website, password))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('show_passwords'))
+    return redender_template('add_password.html')
+
+@app.route('/show_passwords')
+def show_passwords():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT website, password FROM passwords')
+    passwords = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('show_passwords.html', passwords=passwords)
+    
+        
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=8080 )
       
